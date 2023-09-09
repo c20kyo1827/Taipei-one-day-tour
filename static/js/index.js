da49b88e54516be132bd8b197145183e2129fa8d
c20kyo1827 <33478517+c20kyo1827@users.noreply.github.com>
2023-09-09 11:10:49 +0800

    basic layout index.html

---
+++ static/js/index.js
@@ -0,0 +1,33 @@
+// Function definition
+// Initialization
+function initializeMrt(){
+    ;
+}
+
+function initializeAttraction(){
+    ;
+}
+
+// Mrt
+function scrollToLeft(){
+
+}
+
+function scrollToRight(){
+    
+}
+
+function searchForMrt(){
+    
+}
+
+// Attraction
+function loadingNextPage(){
+
+}
+
+// main
+initializeMrt();
+initializeAttraction();
+
+const searchForm = document.querySelector('.slogan form')
