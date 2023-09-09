7d56eccac038295d00f686b624572c5e35679be2
c20kyo1827 <33478517+c20kyo1827@users.noreply.github.com>
2023-09-09 17:40:02 +0800

    basic layout index.html

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,46 @@
 // Function definition
 // Initialization
 function initializeMrt(){
-    ;
+    console.log("Pre-fetch and initialize the mrts");
+    let fetchURL ="/api/mrts";
+    fetch(fetchURL)
+    .then(response => {
+        return response.json();
+    })
+    .then(json => {
+        console.log(json.data);
+        const board = document.querySelector(".main__mrt-board");
+        json.data.forEach(mrt => {
+            console.log(mrt);
+            const container = document.createElement("div");
+            container.classList.add("main__mrt-element");
+            container.innerText = mrt;
+            board.append(container);
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    })
 }
 
 function initializeAttraction(){
-    ;
+    console.log("Pre-fetch and initialize the attractions");
+    let fetchURL ="/api/attractions?page=0";
+    fetch(fetchURL)
+    .then(response => {
+        return response.json();
+    })
+    .then(json => {
+        console.log(json.data);
+        const board = document.querySelector(".main__attraction-board");
+        console.log(board.nextPage);
+        json.data.forEach(info => {
+            console.log(info);
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    })
 }
 
 // Mrt
@@ -30,4 +65,3 @@ function loadingNextPage(){
 initializeMrt();
 initializeAttraction();
 
-const searchForm = document.querySelector('.slogan form')
