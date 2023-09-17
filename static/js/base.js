1365773ae08354356ed1ea988b74db9d11b324d6
c20kyo1827 <33478517+c20kyo1827@users.noreply.github.com>
2023-09-18 04:41:16 +0800

    basic control button of sign-in/sign-up + need to resize

--- static/js/base.js
+++ static/js/base.js
@@ -13,7 +13,42 @@ baseNamespace.addElementListener = function addBaseElementListener(){
     });
 
     document.querySelector(".navigation__right-option-sign").addEventListener("click", () => {
-        console.log("test");
+        document.querySelector("#sign-container__sign-in.sign-container").style.display = "flex";
+        document.querySelector("#sign-container__sign-up.sign-container").style.display = "none";
+    });
+
+    console.log(document.querySelector("#sign-container__sign-in.sign-container"));
+    console.log(document.querySelector("#sign-container__sign-up.sign-container"));
+
+    let closeButtons = document.querySelectorAll(".sign-box__close-button");
+    [].forEach.call(closeButtons, function(button){
+        button.addEventListener("click", () => {
+            document.querySelector("#sign-container__sign-in.sign-container").style.display = "none";
+            document.querySelector("#sign-container__sign-up.sign-container").style.display = "none";
+        });
+    });
+
+    let messages = document.querySelectorAll(".sign-box__message");
+    [].forEach.call(messages, function(msg){
+        msg.addEventListener("click", () => {
+            const idName = msg.parentElement.parentElement.parentElement.id;
+            if(idName === "sign-container__sign-in"){
+                document.querySelector("#sign-container__sign-in.sign-container").style.display = "none";
+                document.querySelector("#sign-container__sign-up.sign-container").style.display = "flex";
+                const container = document.querySelector(".sign-container__sign-box");
+                console.log(container);
+                // window.getComputedStyle(container).height = "275px";
+                console.log(window.getComputedStyle(container).height);
+            }
+            if(idName === "sign-container__sign-up"){
+                document.querySelector("#sign-container__sign-in.sign-container").style.display = "flex";
+                document.querySelector("#sign-container__sign-up.sign-container").style.display = "none";
+                const container = document.querySelector(".sign-container__sign-box");
+                console.log(container);
+                // window.getComputedStyle(container).height = "322px";
+                console.log(window.getComputedStyle(container).height);
+            }
+        });
     });
 }
 
@@ -21,5 +56,6 @@ baseNamespace.moveBasedOneFix = function moveBasedOneFix(){
     let header = document.querySelector(".header");
     let headerCSS = window.getComputedStyle(header);
     let sibling = header.nextElementSibling;
+    if(window.getComputedStyle(sibling).position === "fixed") return;
     sibling.style.marginTop = headerCSS.height;
 }
