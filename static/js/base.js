6361deace5e9f9f9ed1d41e2e70f439901ccd501
c20kyo1827 <33478517+c20kyo1827@users.noreply.github.com>
2023-09-23 13:30:58 +0800

    js for sign-in/sign-up page

--- static/js/base.js
+++ static/js/base.js
@@ -16,18 +16,16 @@ baseNamespace.addElementListener = function addBaseElementListener(){
     // Sign-in/Sign-up
     // Initialize
     document.querySelector(".navigation__right-option-sign").addEventListener("click", () => {
-        document.querySelector("#sign-container__sign-in.sign-container").classList.add("sign-container--show");
-        document.querySelector("#sign-container__sign-up.sign-container").classList.remove("sign-container--show");
+        baseNamespace.toggleMessageBox("sign-container__sign-up");
     });
 
     // Close
     const closeButtons = document.querySelectorAll(".sign-box__close-button");
     [].forEach.call(closeButtons, function(button){
         button.addEventListener("click", () => {
-            document.querySelector("#sign-container__sign-in.sign-container").classList.remove("sign-container--show");
-            document.querySelector("#sign-container__sign-up.sign-container").classList.remove("sign-container--show");
             const rootId = button.parentElement.parentElement.id;
             baseNamespace.removeMessage(rootId);
+            baseNamespace.hideMessageBox();
         });
     });
 
@@ -36,14 +34,8 @@ baseNamespace.addElementListener = function addBaseElementListener(){
     [].forEach.call(messages, function(msg){
         msg.addEventListener("click", () => {
             const rootId = msg.parentElement.parentElement.parentElement.id;
-            if(rootId === "sign-container__sign-in"){
-                document.querySelector("#sign-container__sign-in.sign-container").classList.remove("sign-container--show");
-                document.querySelector("#sign-container__sign-up.sign-container").classList.add("sign-container--show");
-            }
-            if(rootId === "sign-container__sign-up"){
-                document.querySelector("#sign-container__sign-in.sign-container").classList.add("sign-container--show");
-                document.querySelector("#sign-container__sign-up.sign-container").classList.remove("sign-container--show");
-            }
+            baseNamespace.removeMessage(rootId);
+            baseNamespace.toggleMessageBox(rootId);
         });
     });
 
@@ -70,7 +62,7 @@ baseNamespace.handleSign = function handleSign(rootId){
         const password = document.querySelector("input[name='password'][id='sign-box__form-sign-in']").value;
         console.log(email);
         if(email=="" || password==""){
-            baseNamespace.addMessage(rootId, "信箱或密碼不可為空");
+            baseNamespace.addMessage(rootId, "信箱或密碼不可為空", true);
             return;
         }
     }
@@ -79,22 +71,46 @@ baseNamespace.handleSign = function handleSign(rootId){
         const email = document.querySelector("input[name='email'][id='sign-box__form-sign-up']").value;
         const password = document.querySelector("input[name='password'][id='sign-box__form-sign-up']").value;
         if(name=="" || email=="" || password==""){
-            baseNamespace.addMessage(rootId, "姓名或信箱或密碼不可為空");
+            baseNamespace.addMessage(rootId, "姓名或信箱或密碼不可為空", true);
             return;
         }
         const pattern = /^[0-9a-zA-Z][0-9a-zA-Z.]+@[0-9a-zA-Z]+\.[a-zA-Z]{2,}$/;
         if(!pattern.test(email)){
-            baseNamespace.addMessage(rootId, "無效的信箱");
+            baseNamespace.addMessage(rootId, "無效的信箱", true);
             return;
         }
     }
 }
 
-baseNamespace.addMessage = function addMessage(rootId, content){
+baseNamespace.toggleMessageBox = function toggleMessageBox(rootId){
+    if(rootId === "sign-container__sign-in"){
+        document.querySelector("#sign-container__sign-in.sign-container").classList.remove("sign-container--show");
+        document.querySelector("#sign-container__sign-up.sign-container").classList.add("sign-container--show");
+    }
+    if(rootId === "sign-container__sign-up"){
+        document.querySelector("#sign-container__sign-in.sign-container").classList.add("sign-container--show");
+        document.querySelector("#sign-container__sign-up.sign-container").classList.remove("sign-container--show");
+    }
+}
+
+baseNamespace.hideMessageBox = function hideMessageBox(){
+    document.querySelector("#sign-container__sign-in.sign-container").classList.remove("sign-container--show");
+    document.querySelector("#sign-container__sign-up.sign-container").classList.remove("sign-container--show");
+}
+
+baseNamespace.addMessage = function addMessage(rootId, content, isFail){
+    if(content === "") return;
     let changeMsg = document.querySelectorAll("#sign-box__message-info");
     [].forEach.call(changeMsg, function(msg){
-        if(msg.parentElement.parentElement.parentElement.id === rootId){
-            baseNamespace.addBoxHeight(rootId, msg.innerText);
+        if(msg.parentElement.parentElement.parentElement.id === rootId){``
+            // Check box content
+            if(msg.innerText === "")
+                baseNamespace.addBoxHeight(rootId);
+            // Color format
+            if(isFail)
+                msg.classList.add("sign-box__message--fail");
+            else
+                msg.classList.add("sign-box__message--success");
             msg.classList.add("sign-box__message--show");
             msg.classList.remove("sign-box__message--hide");
             msg.innerText = content;
@@ -105,16 +121,18 @@ baseNamespace.addMessage = function addMessage(rootId, content){
 baseNamespace.removeMessage = function removeMessage(rootId){
     let changeMsg = document.querySelectorAll("#sign-box__message-info");
     [].forEach.call(changeMsg, function(msg){
-        baseNamespace.subBoxHeight(rootId, msg.innerText);
+        if(msg.innerText === "") return;
+        baseNamespace.subBoxHeight(rootId);
         msg.classList.remove("sign-box__message--show");
+        msg.classList.remove("sign-box__message--fail");
+        msg.classList.remove("sign-box__message--success");
         msg.classList.add("sign-box__message--hide");
         msg.innerText = "";
     });
     
 }
 
-baseNamespace.addBoxHeight = function addBoxHeight(rootId, content){
-    if(content !== "") return;
+baseNamespace.addBoxHeight = function addBoxHeight(rootId){
     let changeMsg = document.querySelectorAll(".sign-box__message--cursor");
     [].forEach.call(changeMsg, function(msg){
         console.log(rootId + " " + msg);
@@ -128,8 +146,7 @@ baseNamespace.addBoxHeight = function addBoxHeight(rootId, content){
     });
 }
 
-baseNamespace.subBoxHeight = function subBoxHeight(rootId, msgCurContent){
-    if(msgCurContent === "") return;
+baseNamespace.subBoxHeight = function subBoxHeight(rootId){
     let changeMsg = document.querySelectorAll(".sign-box__message--cursor");
     [].forEach.call(changeMsg, function(msg){
         if(msg.parentElement.parentElement.parentElement.id === rootId){
